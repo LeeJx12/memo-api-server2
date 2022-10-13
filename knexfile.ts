fdc87ea63610650ae69797f40b82979a617843b2
LeeJx2 <46637231+LeeJx12@users.noreply.github.com>
2022-10-13 14:26:01 +0900

    Feat: dotenv 추가, postgresql 접속정보 추가
    dotenv 추가, postgresql 접속정보 추가

--- knexfile.ts
+++ knexfile.ts
@@ -2,13 +2,16 @@ import path from 'path';
 import Knex from 'knex';
 
 const BASE_PATH = path.join(__dirname, 'src', 'server', 'db');
+const { PG_HOST, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
 
 export const knex = Knex({
   client: 'postgresql',
   connection: {
-    database: 'memo',
-    user: 'memo',
-    password: 'a'
+    host: PG_HOST,
+    port: 5432,
+    database: PG_DATABASE,
+    user: PG_USERNAME,
+    password: PG_PASSWORD
   },
   pool: {
     min: 2,
